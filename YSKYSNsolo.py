f812f5ed3bdad6de3a3343dbbf7f7b24d6d9121b
the muffin man <93288617+Muffinlavania@users.noreply.github.com>
2023-10-12 23:37:14 -0400

    Update YSKYSNsolo.py
    yae

--- YSKYSNsolo.py
+++ YSKYSNsolo.py
@@ -617,6 +617,59 @@ def thingthing(key):
 YL='''\nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\no----------------------GGGGGGGGGGGGG--------------------------o\no--------------------GGGGGGGGGGGGGGGG-------------------------o\no--------------------GbbbbbbbbbbbbbGGG------------------------o\no--------------------bbbbbbbbbbbbbbbGG------------------------o\no--------------------bbbWWWbbbbWWWbbbB------------------------o\no--------------------bbbbbbbbbbbbbbbbB------------------------o\no---------------------bbbbbbbbbbbbbbB-------------------------o\no---------------------BbbbmmmmmbbbBB--------------------------o\no----------------------BbbbbbbbbbBB---------------------------o\no-----------------------BBBBBBBBBBb---------------------------o\no-------------------nnnnbbbbbbbbbbbnnn------------------------o\no---------------nnnnnnnnnnnnnnnnnnnnnnn-----------------------o\no--------------nnnnnnnnnnnnnnnnnnnnnnnnn----------------------o\no--------------nnnnnnnnnnnnnnnnnnnnnnnnnnn--------------------o\no-------------nnnnnnnnnnnnnnnnnnnnnnnnnnnnn-------------------o\no-------------nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn------------------o\no-------------nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn------------------o'''
 YS='''\nooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\no--__gggwwgg_--------rrGGGGGGGGGGGGGr---------------__gwg__---o\no--__ggwwwgg_-------rGGGGGGGGGGGGGGGGr--------------_gwwgg_---o\no--__gwwggg_--------rGBbbbbbbbbbbbBGGGr------------__ggwwg_---o\no--__ggwwggg_-------rBBBBBBbbbBBBBBBGGr-----------__gggwwg_---o\no--__gggwwwg__------rbbbWWWbbbbWWWbbbr------------_ggwwwgg_---o\no--__ggggwwgg_------rbbbbbbbbbbbbbbbbr----------___gwwwgg__---o\no-__gggwwwwgg_------rrbbbbbbbbbbbbbbrr----------_ggwgwwgg__---o\no-__ggwwwgggg_-------rrbbbmmmmmbbbBrr----------__gwwggwwgg__--o\no-__gggwwwgg_---------rrBbbbbbbbBBBr-----------__gwwgggwwgg__-o\no-__ggggwwwgg_-----rrrrnBBBBBBBBBbbrrr-----_____ggwwgggwgwgg_-o\no--_gggwwggg_--rrrrrnnnnnbbbbbbbbbnnnnrr____gggggwwwggwggwwgg_o\no-_gggwwgg____rrnnnnnwwwwwwnnnnnnnnnnnnrrggggwwwwggwgwggggwwggo\no__ggwwgggggggrnnwwwwwwwwwwwwwwwnnnnnnnnrrwwwwggggggwggg_ggwwwo\no__gwwggggggwwwwwwwnnnnnnnnnnnwwwwnnnnnwwwwrrggggwwwwwwgg_ggggo\no_gggwwwwwwwwwwnnnnnnnnnnnnnnnnnnwwwwwwwnnnnrwwwwwwgggwwgg___-o\no-_gggwwgggggrnnnnnnnnnnnnnnnnnnnnnnwwwwwwwwwggggggg_ggwwgg_--o\no-__gggggg___rnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnrgg_______ggwwg_--o'''
 
+
+ITEMS = """-----------------------------------------------------------------
+-ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo-
+-o                      lllllllllllllllll                      o-
+-o                     lllllllllllllllllll                     o-
+-o                    lllllllllllllllllllll                    o-
+-o                   lllttlllbbbbbbbbbblllll                   o-
+-o                  llllllabbbllllllsssllllll                  o-
+-o                lllllllbbbaallllsslllllllllll                o-
+-o               lllllllbblllaalssllllllllllllll               o-
+-o              llllllllblllllsslllllllllllllllll              o-
+-o              llllllllblllsslllllllllllllllllll              o-
+-o          llllllllllllbssslllllllllllllllllllllllll          o-
+-o       lllllllllllllllllllllllllllllllllllllllllllllll       o-
+-o   ggggggggggggggggggggggggggggggggggggggggggggggggggggggg   o-
+-ogggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggo-
+-ogggSSRRggggggggggggggVVVgggggggggggggOOOgggggggggHgggggggggggo-
+-ogggRRgggggggggggggggVVVVVggggP=PggggOOOOOgggggggJJggggggZggggo-
+-oggggggggggGGGGggggggg111ggggggggggggg222gggggggJJgggggCCCCCggo-
+-oggBLgggggggggggggggggg1ggggggggggggggg2gggggggggggggggcccccggo-
+-ogAAAgggggggggggggggggggggggggggggggggggggggggggggggggggggggggo-
+-ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo-
+-----------------------------------------------------------------"""
+ITEMSd = {
+  '-':" ",
+  " " : '"\033[48;5;52m " if "Red Pill" in iinv else "\033[48;5;17m " if "Blue Pill" in iinv else " "',
+  'o' : '"\033[48;5;0m " if not HEAVEN_LIGHT else "\033[48;5;234m "',
+  'l' : '"\033[48;5;7m " if HEAVEN_LIGHT else eval(ITEMSd[" "])',
+  'g' : '\033[48;5;242m ', 
+  's' : '"\033[48;5;100m " if HEAVEN_BOW else eval(ITEMSd["l"])', #bow string
+  'b' : '"\033[48;5;226m " if HEAVEN_BOW else eval(ITEMSd["l"]) ', #bow well bow
+  'a' : '"\033[48;5;231m " if HEAVEN_ARROW else eval(ITEMSd["l"]) ', #arrow shaft
+  't' : '"\033[48;5;195m " if HEAVEN_ARROW else eval(ITEMSd["l"]) ', #arrow tip
+  'A': '"\033[48;5;196m " if "Apple" in iinv else ITEMSd["g"]', #apple red
+  'B': '"\033[48;5;131m " if "Apple" in iinv else ITEMSd["g"]', #apple brown (stem)
+  'L': '"\033[48;5;112m " if "Apple" in iinv else ITEMSd["g"]', #apple leaf
+  'P': '"\033[48;5;124m " if "Red Pill" in iinv else "\033[48;5;27m " if "Blue Pill" in iinv else ITEMSd["g"]',#placebo pill
+  '=': '"\033[48;5;253m " if "Red Pill" in iinv or "Blue Pill" in iinv else ITEMSd["g"]',#placebo middle
+  'R': '"\033[48;5;160m " if "Cherry" in iinv else ITEMSd["g"]', #cherry red
+  'S': '"\033[48;5;64m " if "Cherry" in iinv else ITEMSd["g"]', #cheery stem/connector
+  'C': '"\033[48;5;230m " if "Cheesecake" in iinv else ITEMSd["g"]', #cheesecake top
+  'c': '"\033[48;5;222m " if "Cheesecake" in iinv else ITEMSd["g"]', #cheesecake bottom
+  'Z': '"\033[48;5;162m " if "Cheesecake" in iinv else ITEMSd["g"]', #cheesecake strawberry
+  'J': '"\033[48;5;1m " if "Jalepeno" in iinv else ITEMSd["g"]', #jalepeno red
+  'H': '"\033[48;5;28m " if "Jalepeno" in iinv else ITEMSd["g"]', #jalepeno green
+  'G': '"\033[48;5;207m " if "Gum" in iinv else ITEMSd["g"]', #gum pink
+  'V': '"\033[48;5;255m " if "Vanilla Cone" in iinv else ITEMSd["g"]', #vanilla ice cream
+  'O': '"\033[48;5;94m " if "Chocolate Cone" in iinv else ITEMSd["g"]', #chocolate ice cream
+  '1': '"\033[48;5;215m " if "Vanilla Cone" in iinv else ITEMSd["g"]', #vanilla cone
+  '2': '"\033[48;5;215m " if "Chocolate Cone" in iinv else ITEMSd["g"]' #choc cone
+
+}  
+
 r='\033[0m'
 #vars you can change in settings, preferences
 centerit,centermodes,skipintro = "center",["center","right","left"],False # make settings!/hell mode things    
@@ -634,7 +687,8 @@ def yskysn():
   JUSTUPIT,kys,itsafirst,hasspidy=False,False,not acheck("LYS"),False #itsafirst first attacking turn, 2.6 spidy cycle
   zeeeee,whereheat,dmgmul=0,0,1
   noheal,xtreme,bmulti,hell,hell2,cloud9,cancer,nonr=False,False,1,False,False,False,False,False #different modes
-  
+  HEAVEN_LIGHT,HEAVEN_BOW,HEAVEN_ARROW = False,False,False
+  iinv = ['Apple','Cherry','Cheesecake',"Jalepeno","Gum",'Vanilla Cone','Chocolate Cone']
   coloreddict={
     'o':'\033[48;5;0m ', #black for the border
     '-':'\033[48;5;233m ', #black (background)
@@ -822,11 +876,12 @@ yBack#
   #------------------------- End Save/load data stuff ------------------------------
   
   
-  buttons='''
+  buttons=f'''
    !╓─────────╖  R @╓─────────╖      R #╓─────────╖    R$╓─────────╖
-   !║  SPEAK  ║ R  @║  MAGIC  ║      R #║ HEAL UP ║   R $║   KYS   ║
+   !║  SPEAK  ║ R  @║  {"MAGIC" if not hell else "PRAY "}  ║      R #║ {"HEAL UP" if not hell else " ITEM "} ║   R $║   KYS   ║
    !╙─────────╜ R  @╙─────────╜     R  #╙─────────╜  R  $╙─────────╜
   '''
+  
   #thresholds: 600, 300
   #for health: 750, 500, 250
   
